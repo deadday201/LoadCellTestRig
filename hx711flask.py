6fc831731d512db11e605b4750e5b32c3fe938d4
deadday201 <9267746+deadday201@users.noreply.github.com>
2017-11-11 13:13:51 -0600

    Update hx711flask.py

--- hx711flask.py
+++ hx711flask.py
@@ -7,3 +7,19 @@ from hx711 import HX711
 from flask import Flask, request, redirect, render_template
 
 def cleanAndExit():
+	print ("cleaning...")
+	GPIO.cleanup()
+	print ("bye")
+	sys.exit()
+
+hx = HX711(5,6)
+hx.set_reading_format("LSB", "MSB")
+#calibrate to generate the reference unit value
+hx.set_reference_unit(1)
+hx.reset()
+hx.tare()
+datafile = open('loadData.csv', 'w')
+app = Flask(__name__)
+@app.route('/')
+def hiThere():
+	return render_template('index.html')
